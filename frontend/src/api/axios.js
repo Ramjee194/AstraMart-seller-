06fa30d4a1661a5b247afb9782eced79a3ef2cdc
Ramjee194 <ramjeekumaryadav733@gmail.com>
2025-11-19 00:14:39 +0530

    updated

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://astramart1.onrender.com/'
+    baseURL: 'https://astramart1.onrender.com'
 });
 
 API.interceptors.request.use((req) => {
